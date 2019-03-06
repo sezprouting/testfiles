121b09e807574bac4d9d618e2aba86071f706a7f
Sez Flynn <SezF@provoke.co.nz>
2019-03-07 10:34:12 +1300

    receive and print webhook events

--- GitHubFunctionApp/GithubHttpTrigger.cs
+++ GitHubFunctionApp/GithubHttpTrigger.cs
@@ -5,18 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Octokit;
+using Octokit.Internal;
 
 namespace GitHubFunctionApp
 {
     public static class GithubHttpTrigger
     {
         [FunctionName(nameof(GithubWebhook))]
-        public static HttpResponseMessage GithubWebhook(
+        public static async Task<HttpResponseMessage> GithubWebhook(
             [HttpTrigger("POST", WebHookType = "github")]
             HttpRequestMessage req,
             TraceWriter log)
         {
-            log.Info("GithubWebhook has been triggered.");
+
+            log.Info("GithubWebhook has been triggered. Request content follows:");
+            string json = await req.Content.ReadAsStringAsync();
+            log.Info(json);
+
+            var serializer = new SimpleJsonSerializer();
+            ActivityPayload payload = serializer.Deserialize<ActivityPayload>(json);
+
             return req.CreateResponse(HttpStatusCode.NoContent);
         }
     }
